6c0278d40fca708aea8143684132363d30aa42a4
David Godoy <dgodoyrodri@gmail.com>
2023-05-21 00:19:56 +0200

    Created first version of component and lwc page. Offer retrieval

---
+++ force-app/main/default/lwc/offerDetails/offerDetails.js
@@ -0,0 +1,31 @@
+import { LightningElement, track } from 'lwc';
+import getOfferBackend from '@salesforce/apex/RecruitmentChatController.getOffer';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class OfferDetails extends LightningElement {
+    offerId;
+    @track offer;
+
+    inputChange(event) {
+        this.offerId = event.target.value;
+    }
+
+    getOffer(event) {
+        getOfferBackend({offerId: this.offerId})
+        .then(result =>{
+            this.offer = result;
+        })
+        .catch(error =>{
+            const evt = new ShowToastEvent({
+                title: 'Error on offer component',
+                message: error.body.message,
+                variant: 'error',
+            });
+            this.dispatchEvent(evt);
+            console.error(error);
+        })
+
+
+    }
+
+}
